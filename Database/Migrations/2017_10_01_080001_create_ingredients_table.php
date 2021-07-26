e17231a15daa63cf74a45f6f83a62dfcf8c0acf9
marco76tv <marco.sottana@gmail.com>
2021-07-26 09:16:13 +0200

    .

--- Database/Migrations/2017_10_01_080001_create_ingredients_table.php
+++ Database/Migrations/2017_10_01_080001_create_ingredients_table.php
@@ -1,36 +1,37 @@
 <?php
+
+declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-
 //---- models ---
 use Modules\Food\Models\Ingredient as MyModel;
 
-class CreateIngredientsTable extends Migration{
+class CreateIngredientsTable extends Migration {
     //protected $table = 'blog_post_ingredients'; // potrebbe essere un ristorante, un ufficio etc etc
-    public function getTable(){
+    public function getTable() {
         return with(new MyModel())->getTable();
     }
+
     /**
      * Run the migrations.
      */
-    public function up()
-    {
-        if (!Schema::hasTable($this->getTable())) {
-            Schema::create($this->getTable(), function (Blueprint $table) {
-                $table->increments('post_id');//->primary();
+    public function up(): void {
+        if (! Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table): void {
+                $table->increments('id'); //->primary();
                 $table->string('created_by');
                 $table->string('updated_by');
-                $table->string('deleted_by');
+                //$table->string('deleted_by');
                 $table->ipAddress('created_ip');
                 $table->ipAddress('updated_ip');
-                $table->ipAddress('deleted_ip'); //$table->ipAddress('visitor');	IP address equivalent column.
+                //$table->ipAddress('deleted_ip'); //$table->ipAddress('visitor');	IP address equivalent column.
                 $table->timestamps();
-                $table->softDeletes();
+                //$table->softDeletes();
             });
         }
-        
-        Schema::table($this->getTable(), function (Blueprint $table) {
-            $table->increments('post_id')->change();
+
+        Schema::table($this->getTable(), function (Blueprint $table): void {
+            //$table->increments('post_id')->change();
             //->autoIncrement()
         });
     }
@@ -38,8 +39,7 @@ class CreateIngredientsTable extends Migration{
     /**
      * Reverse the migrations.
      */
-    public function down()
-    {
+    public function down(): void {
         if (Schema::hasTable($this->getTable())) {
             Schema::drop($this->getTable());
         }
