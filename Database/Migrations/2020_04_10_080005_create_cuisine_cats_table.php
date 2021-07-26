070f54d6e67731080d3da4e8379e74fee6cdd0ad
marco76tv <marco.sottana@gmail.com>
2021-07-26 09:48:09 +0200

    .

--- Database/Migrations/2020_04_10_080005_create_cuisine_cats_table.php
+++ Database/Migrations/2020_04_10_080005_create_cuisine_cats_table.php
@@ -1,52 +1,51 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-//--- models --
-use Modules\Food\Models\CuisineCat as MyModel;
+declare(strict_types=1);
 
+use Illuminate\Database\Schema\Blueprint;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
 /**
- * Class CreateCuisineCatsTable
+ * Class CreateCuisineCatsTable.
  */
-class CreateCuisineCatsTable extends Migration
-{
-    /**
-     * @return mixed
-     */
-    public function getTable()
-    {
-        return with(new MyModel())->getTable();
-    }
-
+class CreateCuisineCatsTable extends XotBaseMigration {
     /**
      * Run the migrations.
      */
-    public function up()
-    {
-        if (! Schema::hasTable($this->getTable())) {
-            Schema::create($this->getTable(), function (Blueprint $table) {
-                $table->increments('id'); //->primary();
-                $table->string('day_name');
-                $table->integer('day_of_week');
-                $table->time('open_at'); //time or timeTz ??
-                $table->time('close_at');
-                $table->boolean('is_closed')->nullable();  //field from closed to is_closed is more readable
-                $table->text('note')->nullable();
-                $table->string('created_by')->nullable();
-                $table->string('updated_by')->nullable();
-                $table->string('deleted_by')->nullable();
+    public function up(): void {
+        //-- CREATE --
+        if (! $this->tableExists()) {
+            $this->getConn()->create(
+                $this->getTable(),
+                function (Blueprint $table): void {
+                    $table->increments('id'); //->primary();
+                    $table->string('day_name');
+                    $table->integer('day_of_week');
+                    $table->time('open_at'); //time or timeTz ??
+                    $table->time('close_at');
+                    $table->boolean('is_closed')->nullable();  //field from closed to is_closed is more readable
+                    $table->text('note')->nullable();
+                    $table->string('created_by')->nullable();
+                    $table->string('updated_by')->nullable();
 
-                $table->ipAddress('created_ip')->nullable();
-                $table->ipAddress('updated_ip')->nullable();
-                $table->ipAddress('deleted_ip')->nullable(); //$table->ipAddress('visitor');	IP address equivalent column.
-
-                $table->timestamps();
-               // $table->softDeletes();
-            });
+                    $table->timestamps();
+                    // $table->softDeletes();
+                }
+            );
         }
-        Schema::table($this->getTable(), function (Blueprint $table) {
+
+        //-- UPDATE --
+        $this->getConn()->table(
+            $this->getTable(),
+            function (Blueprint $table): void {
+                if (! $this->hasColumn('post_id')) {
+                    $this->renameColumn('post_id', 'id');
+                }
+            }
+        );
+
+        /*
+        Schema::table($this->getTable(), function (Blueprint $table): void {
             if (! Schema::hasColumn($this->getTable(), 'status')) {
                 $table->integer('status')->nullable()->after('post_id');
             }
@@ -54,15 +53,6 @@ class CreateCuisineCatsTable extends Migration
                 $table->renameColumn('post_id', 'id');
             }
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
-    {
-        if (Schema::hasTable($this->getTable())) {
-            Schema::drop($this->getTable());
-        }
+        */
     }
 }
