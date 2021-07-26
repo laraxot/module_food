c19e87060e272ae2b1d662dccefcd56c8e7d516e
marco76tv <marco.sottana@gmail.com>
2021-07-26 09:24:20 +0200

    .

--- Database/Migrations/2018_01_31_080003_create_cuisine_cats_table.php
+++ Database/Migrations/2018_01_31_080003_create_cuisine_cats_table.php
@@ -1,26 +1,25 @@
 <?php
+
+declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-
 //--- models --
 use Modules\Food\Models\CuisineCat as MyModel;
 
-class CreateCuisineCatsTable extends Migration
-{
+class CreateCuisineCatsTable extends Migration {
     //protected $table = 'blog_post_cuisine_cats'; // potrebbe essere un ristorante, un ufficio etc etc
 
-    public function getTable()
-    {
+    public function getTable() {
         return with(new MyModel())->getTable();
     }
+
     /**
      * Run the migrations.
      */
-    public function up()
-    {
-        if (!Schema::hasTable($this->getTable())) {
-            Schema::create($this->getTable(), function (Blueprint $table) {
-                $table->increments('id');//->primary();
+    public function up(): void {
+        if (! Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table): void {
+                $table->increments('id'); //->primary();
                 $table->string('day_name');
                 $table->integer('day_of_week');
                 $table->time('open_at'); //time or timeTz ??
@@ -39,23 +38,15 @@ class CreateCuisineCatsTable extends Migration
                 $table->softDeletes();
             });
         }
-        Schema::table($this->getTable(), function (Blueprint $table) {
-
+        Schema::table($this->getTable(), function (Blueprint $table): void {
             //----------- ADD -----------
-            if (!Schema::hasColumn($this->getTable(), 'created_by')) {
+            if (! Schema::hasColumn($this->getTable(), 'created_by')) {
                 $table->string('created_by')->nullable();
             }
-            if (!Schema::hasColumn($this->getTable(), 'updated_by')) {
+            if (! Schema::hasColumn($this->getTable(), 'updated_by')) {
                 $table->string('updated_by')->nullable();
             }
             //-------- CHANGE ----------------
-            $schema_builder = Schema::getConnection()
-                ->getDoctrineSchemaManager()
-                ->listTableDetails($table->getTable());
-
-            if (!$schema_builder->hasIndex($this->getTable().'_'.'post_id'.'_unique')) {
-                $table->integer('post_id')->ignore()->index()->unique()->change();
-            }
 
             $table->string('created_by')->nullable()->change();
             $table->string('updated_by')->nullable()->change();
@@ -77,9 +68,14 @@ class CreateCuisineCatsTable extends Migration
                 $table->dropColumn('close_at');
             }
 
+            /*
+            $schema_builder = Schema::getConnection()
+                ->getDoctrineSchemaManager()
+                ->listTableDetails($table->getTable());
 
-
-
+            if (!$schema_builder->hasIndex($this->getTable().'_'.'post_id'.'_unique')) {
+                $table->integer('post_id')->ignore()->index()->unique()->change();
+            }
 
 
 
@@ -96,14 +92,14 @@ class CreateCuisineCatsTable extends Migration
             }
             // $table->dropColumn('id');
             // $table->string('post_id', 16)->autoIncrement()->unsigned()->nullable()->change();
+            */
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down()
-    {
+    public function down(): void {
         if (Schema::hasTable($this->getTable())) {
             Schema::drop($this->getTable());
         }
