c4288373965ffff4d49554be081f74379972bc67
Malebestia <malebestia@yahoo.it>
2021-08-20 17:31:12 +0200

    .

--- Models/Panels/Policies/HomePanelPolicy.php
+++ Models/Panels/Policies/HomePanelPolicy.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Food\Models\Panels\Policies;
 
+use Modules\Xot\Contracts\PanelContract;
+use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 /**
  * Class HomePanelPolicy.
  */
 class HomePanelPolicy extends XotBasePanelPolicy {
+    public function artisan(?UserContract $user, PanelContract $panel): bool {
+        return true;
+    }
 }
