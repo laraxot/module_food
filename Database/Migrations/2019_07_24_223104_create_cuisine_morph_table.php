f84c9e65cc6aaeb305b59d6ce613712ed34fb8f5
marco76tv <marco.sottana@gmail.com>
2021-11-03 20:51:46 +0100

    .

--- Database/Migrations/2019_07_24_223104_create_cuisine_morph_table.php
+++ Database/Migrations/2019_07_24_223104_create_cuisine_morph_table.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
 
 //----- models -----
 
